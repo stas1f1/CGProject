d4fa2db6100dbe6f033d67ddb790876ce78fd2a3
Chumakov SV <stas1f1@gmail.com>
2020-12-29 12:55:24 +0300

    Добавьте файлы проекта.

---
+++ Camera.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.DoubleNumerics;
+using System.Windows.Forms;
+using static RTTest1.Objects;
+using static RTTest1.Utils;
+using System.Diagnostics;
+
+namespace RTTest1
+{
+    public class Camera
+    {
+        public FormUpdater updateDelegate = null;
+        public Point3D pos;
+        public Point[] Screencoords;
+        public Stopwatch stopwatch = new Stopwatch();
+        Color AmbientColor = Color.DarkGray;
+        bool scatteredRays = false;
+
+        public Camera(Point3D pos, Point lu, Point br, bool scatteredRays = false)
+        {
+            this.pos = pos;
+            Screencoords = new Point[]{ lu, br};
+            this.scatteredRays = scatteredRays;
+        }
+
+        public Point3D SetCamVector(int _x, int _y, int w, int h)
+        {
+            int x = _x - w / 2;
+            int y = _y - h / 2;
+            int z = (int)pos.Z;
+            Point3D cameraVector = new Point3D(x, y, -z);
+            double distance = Distance(cameraVector, new Point3D(0, 0, 0));
+            cameraVector.X = cameraVector.X / distance;
+            cameraVector.Y = cameraVector.Y / distance;
+            cameraVector.Z = cameraVector.Z / distance;
+            return cameraVector;
+        }
+
+        public void Render(Bitmap bm, PictureBox pb)
+        {
+            Graphics g = Graphics.FromImage(bm);
+            g.Clear(Color.Transparent);
+
+            Point3D cameraLocation = new Point3D(pos);
+
+            int l = Math.Max(0, Screencoords[0].X);
+            int r = Math.Min(bm.Width, Screencoords[1].X);
+
+            int u = Math.Max(0, Screencoords[0].Y);
+            int d = Math.Min(bm.Height, Screencoords[1].Y);
+
+            int w = r - l;
+            int h = d - u;
+            int total = w * h;
+            stopwatch.Reset();
+            stopwatch.Start();
+            int pDone = 0;
+
+            for (int i = l; i < r; i++)
+                for (int j = u; j < d; j++)
+                {
+                    bm.SetPixel(i, j, RayCast(cameraLocation, SetCamVector(i, j, bm.Width, bm.Height), 3));
+                    pDone++;
+                    updateDelegate(((double)pDone) / total, stopwatch.Elapsed);
+                }
+            stopwatch.Stop();
+            pb.Refresh();
+        }
+
+        public Color RayCast(Point3D pos, Point3D dir, int depth)
+        {
+            if (depth == 0) return Color.Black;
+            Point3D hit = new Point3D();
+            Point3D normal = new Point3D();
+            Material hitMaterial = new Material();
+            double minDist = double.MaxValue;
+            bool found = false;
+            double eps = 0.001;
+
+
+            foreach (Mesh m in meshes)
+            {
+                foreach (Polygon pol in m.faces)
+                {
+                    double t = 0;
+                    Point3D P = new Point3D();
+                    if (!pol.Ray_intersection(pos, dir, ref P, ref t))
+                        continue;
+
+
+                    if (t < minDist)
+                    {
+                        found = true;
+                        minDist = t;
+                        hit = P;
+                        normal = pol.normal;
+                        hitMaterial = m.material;
+                    }
+                }
+            }
+            foreach (Sphere sphere in spheres)
+            {
+                double t = 0;
+                if (sphere.ray_intersection(pos, dir, ref t))
+                {
+                    if (t < minDist)
+                    {
+                        found = true;
+                        minDist = t;
+                        hit = pos + dir * t;
+                        normal = Normalize(hit - sphere.pos);
+                        hitMaterial = sphere.mat;
+                    }
+                }
+            }
+
+            if (found)
+            {
+                double difIntensitySumR = 0;
+                double difIntensitySumG = 0;
+                double difIntensitySumB = 0;
+                double specIntensitySum = 0;
+
+                double ReflectionR = 0;
+                double ReflectionG = 0;
+                double ReflectionB = 0;
+
+                double RefractionR = 0;
+                double RefractionG = 0;
+                double RefractionB = 0;
+
+                Color DiffuseReflectedCol = AmbientColor;
+
+                if (hitMaterial.parameters[3] > 0)
+                {
+                    Point3D RefractRay = Normalize(RayRefract(dir, normal, hitMaterial.refractionIndex));
+                    Point3D RefractLoc = RefractRay * normal < 0 ? hit - normal * eps : hit + normal * eps;
+                    Color RefractCol = RayCast(RefractLoc, RefractRay, depth - 1);
+
+                    RefractionR = RefractCol.R * hitMaterial.parameters[3];
+                    RefractionG = RefractCol.G * hitMaterial.parameters[3];
+                    RefractionB = RefractCol.B * hitMaterial.parameters[3];
+                }
+
+                if (hitMaterial.parameters[2] > 0)
+                {
+                    Point3D ReflectRay = RayReflect(dir, normal);
+                    Point3D ReflectLoc = ReflectRay * normal < 0 ? hit - normal * eps : hit + normal * eps;
+                    Color ReflectCol = RayCast(ReflectLoc, ReflectRay, depth - 1);
+
+                    ReflectionR = ReflectCol.R * hitMaterial.parameters[2] * (1 - hitMaterial.parameters[3]);
+                    ReflectionG = ReflectCol.G * hitMaterial.parameters[2] * (1 - hitMaterial.parameters[3]);
+                    ReflectionB = ReflectCol.B * hitMaterial.parameters[2] * (1 - hitMaterial.parameters[3]);
+                }
+
+                if (hitMaterial.parameters[0] > 0)
+                    if (scatteredRays)
+                    {
+                        List<Point3D> DiffuseRays = MakeDiffuseRays(hit, normal, 4, 8); // 4 8
+                        Point3D DiffuseLoc = DiffuseRays[0] * normal < 0 ? hit - normal * eps : hit + normal * eps;
+                        int[] colSum = new int[3] { 0, 0, 0 };
+
+                        foreach (var DiffuseRay in DiffuseRays)
+                        {
+                            Color curDifCol = RayCast(DiffuseLoc, DiffuseRay, depth - 1);
+                            colSum[0] += curDifCol.R;
+                            colSum[1] += curDifCol.G;
+                            colSum[2] += curDifCol.B;
+                        }
+                        colSum[0] /= DiffuseRays.Count;
+                        colSum[1] /= DiffuseRays.Count;
+                        colSum[2] /= DiffuseRays.Count;
+                        DiffuseReflectedCol = Color.FromArgb(colSum[0], colSum[1], colSum[2]);
+                    }
+                    else
+                    { }
+
+                foreach (Light l in lights)
+                {
+                    Point3D lightVecFull = l.pos - hit;
+                    double light_distance = lightVecFull.Length();
+                    Point3D lightVec = Normalize(lightVecFull);
+
+
+                    Point3D shadow_origin = lightVec * normal < 0 ? hit - normal * eps : hit + normal * eps;
+                    Point3D shadow_destination = new Point3D();
+                    Point3D shadow_N = new Point3D();
+                    Material tmat = new Material();
+                    if (FindIntersection(shadow_origin, lightVec, ref shadow_destination, ref shadow_N, ref tmat) && (shadow_destination - shadow_origin).Length() < light_distance)
+                        continue;
+                    
+                    double intensity = LightFalloff(light_distance, 800) * l.intensity * Math.Max(0.0, (lightVec * normal));
+                    difIntensitySumR += (l.color.R / 255.0) * intensity;
+                    difIntensitySumG += (l.color.G / 255.0) * intensity;
+                    difIntensitySumB += (l.color.B / 255.0) * intensity;
+                    specIntensitySum += Math.Pow(Math.Max(0.0, (RayReflect(lightVec, normal) * dir)), hitMaterial.specularHighlight) * l.intensity;
+                }
+                double lightnessR = difIntensitySumR * (1 - hitMaterial.parameters[2]) * (1 - hitMaterial.parameters[3]);
+                double lightnessG = difIntensitySumG * (1 - hitMaterial.parameters[2]) * (1 - hitMaterial.parameters[3]);
+                double lightnessB = difIntensitySumB * (1 - hitMaterial.parameters[2]) * (1 - hitMaterial.parameters[3]);
+
+                double addspec = 255.0 * specIntensitySum * hitMaterial.parameters[1];
+
+                double colR = hitMaterial.color.R * (1 - hitMaterial.parameters[0]) * lightnessR + DiffuseReflectedCol.R * hitMaterial.parameters[0];
+                double colG = hitMaterial.color.G * (1 - hitMaterial.parameters[0]) * lightnessG + DiffuseReflectedCol.G * hitMaterial.parameters[0];
+                double colB = hitMaterial.color.B * (1 - hitMaterial.parameters[0]) * lightnessB + DiffuseReflectedCol.B * hitMaterial.parameters[0];
+
+                int r = (int)(colR + addspec + ReflectionR + RefractionR);
+                r = r > 255 ? 255 : r;
+                int g = (int)(colG + addspec + ReflectionG + RefractionG);
+                g = g > 255 ? 255 : g;
+                int b = (int)(colB + addspec + ReflectionB + RefractionB);
+                b = b > 255 ? 255 : b;
+                return Color.FromArgb(r, g, b);
+            }
+            return AmbientColor;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace RTTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.elapsedTimeLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(649, 612);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 630);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Render";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 630);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.SaveRender);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(318, 630);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(340, 22);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 11;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(198, 635);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "diffuse reflection";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // elapsedTimeLabel
+            // 
+            this.elapsedTimeLabel.AutoSize = true;
+            this.elapsedTimeLabel.Location = new System.Drawing.Point(315, 655);
+            this.elapsedTimeLabel.Name = "elapsedTimeLabel";
+            this.elapsedTimeLabel.Size = new System.Drawing.Size(44, 13);
+            this.elapsedTimeLabel.TabIndex = 13;
+            this.elapsedTimeLabel.Text = "Время:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 699);
+            this.Controls.Add(this.elapsedTimeLabel);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label elapsedTimeLabel;
+    }
+}
+
